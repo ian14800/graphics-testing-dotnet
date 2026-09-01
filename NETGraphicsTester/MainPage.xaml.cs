baaae1c9649938191a84fd470e7ca8a90b95775f
Ian Maher <imaher@esri.com>
2026-09-01 14:43:25 -0700

    Add style file loading for renderers and symbols

--- NETGraphicsTester/MainPage.xaml.cs
+++ NETGraphicsTester/MainPage.xaml.cs
@@ -38,7 +38,7 @@ namespace NETGraphicsTester
         Graphic? currentlyIdentifiedGraphic;
 
         private const int ModelClassMin = 1;
-        private const int ModelClassMax = 5;
+        private const int ModelClassMax = 9;
         private string currentScene = "Blank";
         private bool initialSetup = true;
 
@@ -165,7 +165,7 @@ namespace NETGraphicsTester
                 switch(currentScene)
                 {
                     case "Heavy":
-                        sceneId = "5e43584386f54533b7c30087d1051343";
+                        sceneId = "9851fee6ec0544e0a0c2884d2297c349";
                         break;
                     case "Medium":
                         sceneId = "e10369820fa04866a16338cce14c0f36";
@@ -277,7 +277,7 @@ namespace NETGraphicsTester
                 double x = extent.XMin + (random.NextDouble() * (extent.XMax - extent.XMin));
                 double y = extent.YMin + (random.NextDouble() * (extent.YMax - extent.YMin));
                 double z = baseZ + ((random.NextDouble() * 2 * zRange) - zRange);
-                MapPoint point = new MapPoint(x, y, z, extent.SpatialReference);
+                MapPoint point = new MapPoint(x, y, z, extent.SpatialReference); 
 
                 Graphic graphic = new Graphic(point);
                 graphic.Attributes["class_value"] = random.Next(ModelClassMin, ModelClassMax + 1);
@@ -487,22 +487,33 @@ namespace NETGraphicsTester
                     }
                     break;
                 case 4:
-                    SimpleMarkerSceneSymbol greenConeSymbol = new SimpleMarkerSceneSymbol(
-                        SimpleMarkerSceneSymbolStyle.Cone,
-                        System.Drawing.Color.ForestGreen,
-                        50,
-                        50,
-                        80,
-                        SceneSymbolAnchorPosition.Bottom);
-                    SimpleRenderer visualVariablesRenderer = new SimpleRenderer(greenConeSymbol);
+                    string symbolFilePath = "D:\\ProjectData\\Testing\\MinShipSymbols_round1_points.stylx";
+                    SymbolStyle _pointSymbols = await SymbolStyle.OpenAsync(symbolFilePath);
 
-                    graphicsOverlay.Renderer = visualVariablesRenderer;
-                    operationTimer.Stop();
-                    if (updateUi)
+                    SymbolStyleSearchParameters searchParams = await _pointSymbols.GetDefaultSearchParametersAsync();
+                    SymbolStyleSearchParameters searchParams1 = new SymbolStyleSearchParameters();
+
+                    IList<SymbolStyleSearchResult> styleResults = await _pointSymbols.SearchSymbolsAsync(searchParams1);
+
+                    UniqueValueRenderer styleUniqueRenderer = new UniqueValueRenderer();
+                    styleUniqueRenderer.FieldNames.Add("class_value");
+
+                    int symbolIndex = 1;
+
+                    foreach(SymbolStyleSearchResult result in styleResults)
                     {
-                        EventTimer.Text = $"{operationTimer.ElapsedMilliseconds} ms // event time";
-                        StatusLabel.Text = $"Renderer set to visual variables.";
+                        // MultilayerPointSymbol multiLayerSym = await result.GetSymbolAsync() as MultilayerPointSymbol;
+                        Symbol multiLayerSym = await result.GetSymbolAsync();
+
+                        styleUniqueRenderer.UniqueValues.Add(new UniqueValue(result.Name, result.Key, multiLayerSym, symbolIndex.ToString()));
+
+                        Debug.WriteLine($"Added symbol to renderer: {result.Name}, type: {multiLayerSym.GetType().Name ?? "Unknown"}");
+
+                        symbolIndex++;
                     }
+
+                    graphicsOverlay.Renderer = styleUniqueRenderer;
+
                     break;
                 default:
                     graphicsOverlay.Renderer = redCircle;
@@ -785,62 +796,90 @@ namespace NETGraphicsTester
                 drawClock.Start();
             }
 
-            SimpleMarkerSymbol greenTriangle = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Triangle, System.Drawing.Color.Green, 40);
-            MultilayerPointSymbol modelSymbol1 = await createModelLayerFromFile("1", 100);
-            MultilayerPointSymbol modelSymbol2 = await createModelLayerFromFile("2", 100);
-            MultilayerPointSymbol modelSymbol3 = await createModelLayerFromFile("3", 100);
-            MultilayerPointSymbol modelSymbol4 = await createModelLayerFromFile("4", 100);
-            MultilayerPointSymbol modelSymbol5 = await createModelLayerFromFile("5", 100);
-            MultilayerPointSymbol pictureSymbol1 = await createPictureLayerFromFile("1", 100);
-            MultilayerPointSymbol pictureSymbol2 = await createPictureLayerFromFile("2", 100);
-            MultilayerPointSymbol pictureSymbol3 = await createPictureLayerFromFile("3", 100);
-            MultilayerPointSymbol pictureSymbol4 = await createPictureLayerFromFile("4", 100);
-            MultilayerPointSymbol pictureSymbol5 = await createPictureLayerFromFile("5", 100);
+            string symbolFilePath = "D:\\ProjectData\\Testing\\MinShipSymbols_round2_points.stylx";
+            SymbolStyle _pointSymbols = await SymbolStyle.OpenAsync(symbolFilePath);
+
+            SymbolStyleSearchParameters searchParams = await _pointSymbols.GetDefaultSearchParametersAsync();
+            // SymbolStyleSearchParameters searchParams1 = new SymbolStyleSearchParameters();
+
+            IList<SymbolStyleSearchResult> styleResults = await _pointSymbols.SearchSymbolsAsync(searchParams);
+
+            //SimpleMarkerSymbol greenTriangle = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Triangle, System.Drawing.Color.Green, 40);
+            //MultilayerPointSymbol modelSymbol1 = await createModelLayerFromFile("1", 100);
+            //MultilayerPointSymbol modelSymbol2 = await createModelLayerFromFile("2", 100);
+            //MultilayerPointSymbol modelSymbol3 = await createModelLayerFromFile("3", 100);
+            //MultilayerPointSymbol modelSymbol4 = await createModelLayerFromFile("4", 100);
+            //MultilayerPointSymbol modelSymbol5 = await createModelLayerFromFile("5", 100);
+            //MultilayerPointSymbol pictureSymbol1 = await createPictureLayerFromFile("1", 100);
+            //MultilayerPointSymbol pictureSymbol2 = await createPictureLayerFromFile("2", 100);
+            //MultilayerPointSymbol pictureSymbol3 = await createPictureLayerFromFile("3", 100);
+            //MultilayerPointSymbol pictureSymbol4 = await createPictureLayerFromFile("4", 100);
+            //MultilayerPointSymbol pictureSymbol5 = await createPictureLayerFromFile("5", 100);
+
+            IList<Symbol> symbolList = new List<Symbol>();
+
+            foreach (SymbolStyleSearchResult result in styleResults)
+            {
+                Symbol symbol = await result.GetSymbolAsync();
+
+                symbolList.Add(symbol);
+            }
+
+            Debug.WriteLine($"Built list of {symbolList.Count} symbols");
+
             for (int i = 0; i < assignCount; i++)
             {
-                int symbolType = random.Next(1, 4);
-                string symbolIndex = random.Next(5, 11).ToString();
+                int symbolCount = symbolList.Count;
+
+                int symbolType = random.Next(1, symbolCount);
+                //string symbolIndex = random.Next(5, 11).ToString();
                 Graphic graphic = graphicsOverlay.Graphics[i];
 
-                switch (symbolType)
-                {
-                    case 1:
-                        graphic.Symbol = greenTriangle;
-                        symbolTypeCounts[0]++;
-                        break;
-                    case 2:
-                        MultilayerPointSymbol modelSymbol = symbolIndex switch
-                        {
-                            "5" => modelSymbol5,
-                            "6" => modelSymbol1,
-                            "7" => modelSymbol2,
-                            "8" => modelSymbol3,
-                            "9" => modelSymbol4,
-                            "10" => modelSymbol5,
-                            _ => modelSymbol1
-                        };
-                        graphic.Symbol = modelSymbol;
-                        symbolTypeCounts[1]++;
-                        break;
-                    case 3:
-                        MultilayerPointSymbol pictureSymbol = symbolIndex switch
-                        {
-                            "5" => pictureSymbol5,
-                            "6" => pictureSymbol1,
-                            "7" => pictureSymbol2,
-                            "8" => pictureSymbol3,
-                            "9" => pictureSymbol4,
-                            "10" => pictureSymbol5,
-                            _ => pictureSymbol1
-                        };
-                        graphic.Symbol = pictureSymbol;
-                        symbolTypeCounts[2]++;
-                        break;
-                    default:
-                        graphic.Symbol = greenTriangle;
-                        break;
-                    
-                }
+                //switch (symbolType)
+                //{
+                //    case 1:
+                //        graphic.Symbol = greenTriangle;
+                //        symbolTypeCounts[0]++;
+                //        break;
+                //    case 2:
+                //        MultilayerPointSymbol modelSymbol = symbolIndex switch
+                //        {
+                //            "5" => modelSymbol5,
+                //            "6" => modelSymbol1,
+                //            "7" => modelSymbol2,
+                //            "8" => modelSymbol3,
+                //            "9" => modelSymbol4,
+                //            "10" => modelSymbol5,
+                //            _ => modelSymbol1
+                //        };
+                //        graphic.Symbol = modelSymbol;
+                //        symbolTypeCounts[1]++;
+                //        break;
+                //    case 3:
+                //        MultilayerPointSymbol pictureSymbol = symbolIndex switch
+                //        {
+                //            "5" => pictureSymbol5,
+                //            "6" => pictureSymbol1,
+                //            "7" => pictureSymbol2,
+                //            "8" => pictureSymbol3,
+                //            "9" => pictureSymbol4,
+                //            "10" => pictureSymbol5,
+                //            _ => pictureSymbol1
+                //        };
+                //        graphic.Symbol = pictureSymbol;
+                //        symbolTypeCounts[2]++;
+                //        break;
+                //    default:
+                //        graphic.Symbol = greenTriangle;
+                //        break;
+
+                //}
+
+                Symbol nextUpSymbol = symbolList[symbolType];
+
+                //Symbol multiLayerSym = await styleResults[symbolType].GetSymbolAsync();
+                //Debug.WriteLine($"Found symbol for index {i}: {styleResults[symbolType].Name}");
+                graphic.Symbol = nextUpSymbol;
 
             }
 
